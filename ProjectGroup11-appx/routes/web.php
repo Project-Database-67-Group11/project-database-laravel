4af31783bfddf8a5daef7956f9f21e82e46b5002
AutsadaWiriya <autsadawiriya@gmail.com>
2024-09-20 21:20:15 +0700

    Add

--- ProjectGroup11-appx/routes/web.php
+++ ProjectGroup11-appx/routes/web.php
@@ -15,6 +15,10 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/store', function () {
+    return "<h1>Welcome to store</h1>";
+});
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
