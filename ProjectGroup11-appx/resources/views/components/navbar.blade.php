347eb14f95af133167170bcd422978d3bc6d0487
AutsadaWiriya <autsadawiriya@gmail.com>
2024-10-02 14:40:44 +0700

    Add navbar
    Add navbar

---
+++ ProjectGroup11-appx/resources/views/components/navbar.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@600&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <div class="h-[100px] bg-[#ffffff] shadow-2xl flex justify-between items-center font-[Oswald]">
+        {{-- logo --}}
+        <div class="ml-[200px]">
+            <a href="/"><img src="{{ asset('img/logo.png') }}" alt="Logo"></a>
+        </div>
+
+        <div class="mr-[200px] flex justify-between items-center gap-10">
+            {{-- Cart Icon --}}
+            <a href="/">
+                <svg class="h-10" xmlns="http://www.w3.org/2000/svg" height="40" width="40"
+                    viewBox="0 0 576 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                    <path
+                        d="M0 24C0 10.7 10.7 0 24 0L69.5 0c22 0 41.5 12.8 50.6 32l411 0c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3l-288.5 0 5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5L488 336c13.3 0 24 10.7 24 24s-10.7 24-24 24l-288.3 0c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5L24 48C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z" />
+                </svg>
+            </a>
+
+            {{-- profile --}}
+            <div>
+                <a href="" class="flex justify-center items-center gap-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="40" width="40"
+                        viewBox="0 0 448 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                        <path
+                            d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512l388.6 0c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304l-91.4 0z" />
+                    </svg>
+                    <h1 class="font-semibold text-xl">
+                        {{ Auth::user()->name }}
+                    </h1>
+                </a>
+            </div>
+        </div>
+
+    </div>
+</body>
+
+</html>
