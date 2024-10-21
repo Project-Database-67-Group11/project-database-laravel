53ca79918ee396869bea29080239b58ddc0c6d29
AutsadaWiriya <autsadawiriya@gmail.com>
2024-10-21 14:55:47 +0700

    Edit
    Add scrollbar-none

--- ProjectGroup11-appx/resources/views/components/navbar.blade.php
+++ ProjectGroup11-appx/resources/views/components/navbar.blade.php
@@ -8,6 +8,11 @@
     <title>@yield('title')</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@100;400&display=swap" rel="stylesheet">
+    <style>
+        ::-webkit-scrollbar {
+            display: none;
+        }
+    </style>
 </head>
 
 <body>
